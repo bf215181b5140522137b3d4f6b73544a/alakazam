e85945044fa5b04c5b33ecd913654e1617933436
phasfly <phasfly@ubuntu.(none)>
2013-12-28 16:02:31 -0600

    Broke-out nav, updated Pyramid to use view_configs, added routes for home, register, and login, and made other misc template changes.

--- alakazam/__init__.py
+++ alakazam/__init__.py
@@ -1,30 +1,44 @@
+# 3rd party
 from pyramid.config import Configurator
 from pyramid_jinja2 import renderer_factory
 from sqlalchemy import engine_from_config
 
+# Local
 from alakazam.models import initialize_sql
 
+def add_routes(config):
+    """
+    Docstring goes here
+
+    :param config: blah blah
+    :returns: blah blah
+    """
+    config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('register', '/register')
+
 def main(global_config, **settings):
-    """ This function returns a WSGI application.
+    """
+    This function returns a WSGI application.
     
     It is usually called by the PasteDeploy framework during 
     ``paster serve`` or ``pserve``.
+
     """
     #SQLAlchemy engine config for main DB 
     #Any setting that begins with 'sqlalchemy.' will be picked up
     db_engine = engine_from_config(settings,'sqlalchemy.')
     #Binding engine to the model
     initialize_sql(db_engine)
-    
     config = Configurator(settings=settings)
-    
     config.include('pyramid_jinja2')
-    
     #The views/routes are added here
     config.add_static_view('static', 'static')
-    
-    config.add_route("my_route",'/')
-    config.add_view('alakazam.views.my_view',
-                    route_name="my_route",renderer="index.jinja2")
-    
+    # Add routes
+    config.include(add_routes)
+    # Scans views.py for view_config[s] 
+    config.scan()
+    #config.add_route("my_route",'/')
+    #config.add_view('alakazam.views.home_view',
+    #                route_name="home", renderer="home.jinja2")
     return config.make_wsgi_app()
--- alakazam/views.py
+++ alakazam/views.py
@@ -1,11 +1,26 @@
+from pyramid.view import view_config
 from pyramid.i18n import TranslationStringFactory
 from alakazam.models import DBSession
 
 
 _ = TranslationStringFactory('Alakazam')
 
-def my_view(request):
+@view_config(route_name='home', renderer='templates/home.jinja2')
+def home_view(request):
+    """
+    Docstring goes here
+
+    """
     session = DBSession()
     #Use session to make queries
     #session.query()
-    return {'project':'Alakazam'}
+    return {'project': 'Alakazam'}
+
+@view_config(route_name='register', renderer='templates/register.jinja2')
+def register_view(request):
+    """
+    Docstring goes here
+
+    """
+    return {'nothing': 'Something'}
+
